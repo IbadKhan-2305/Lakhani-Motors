db4223e52e9362e32dacb0b81e3674a9146fdc31
Ibad Khan <ibadkhan2305@gmail.com>
2025-11-13 16:37:06 +0300

    Update project files

--- script.js
+++ script.js
@@ -24,6 +24,7 @@
       e.preventDefault();
       const target = document.querySelector(this.getAttribute('href'));
       if (target) target.scrollIntoView({ behavior: 'smooth' });
+
       // Close mobile menu on click
       if (navLinks.classList.contains('mobile-active')) {
         navLinks.classList.remove('mobile-active');
@@ -58,11 +59,13 @@ window.addEventListener('load', () => {
 /* ---------- INVENTORY: CARS DATA ---------- */
 const cars = [
   {
-   id: 1,
+    id: 1,
     name: "Toyota Landcruiser Prado TX",
     transmission: "Automatic",
     color: "White",
     model: "2019",
+    mileage: "15,000 km",
+    price: "$60,000",
     folder: "assets/images/cars/TOYOTA LANDCRUISER PRADO",
     images: [
       "IMG_3984.JPG",
@@ -73,464 +76,20 @@ const cars = [
     ]
   },
   {
-   id: 2,
-  name: "Toyota Harrier",
-  transmission: "Automatic",
-  color: "Black",
-  model: "2012",
-  folder: "assets/images/cars/TOYOTA HARRIER 2012",
-  images: [
-    "IMG_3898.JPG",
-    "IMG_3921.JPG",
-    "IMG_3923.JPG",
-    "IMG_3897.JPG",
-    "IMG_3911.JPG",
-    "IMG_3912.JPG",
-    "IMG_3915.JPG",
-    "IMG_3917.JPG"
-  ]
-  },
-  {
-    id: 3,
-    name: "Toyota Landcruiser V8 ZX",
+    id: 2,
+    name: "Mercedes-Benz GLE 350d",
     transmission: "Automatic",
     color: "Black",
-    model: "2018",
-    folder: "assets/images/cars/V8 ZX BLACK CANON",
-    images: [
-      "IMG_1507.JPG",
-      "IMG_1506.JPG",
-      "IMG_1511.JPG",
-      "IMG_1513.JPG",
-      "IMG_1519.JPG",
-      "IMG_1522.JPG",
-      "IMG_1524.JPG"
-    ]
-  },
-  {
-    id: 4,
-    name: "Toyota Landcruiser ZX",
-    transmission: "Automatic",
-    color: "White",
-    model: "2022",
-    folder: "assets/images/cars/ZX WHITE",
-    images: [
-      "IMG_1547.JPG",
-      "IMG_1539.JPG",
-      "IMG_1543.JPG",
-      "IMG_1544.JPG",
-      "IMG_1559.JPG",
-      "IMG_1561.JPG",
-      "IMG_1564.JPG",
-      "IMG_1565.JPG"
-    ]
-  },
-  {
-    id: 5,
-    name: "Lexus LX570",
-    transmission: "Automatic",
-    color: "White",
     model: "2020",
-    folder: "assets/images/cars/LEXUS LX570  CANON",
+    mileage: "12,500 km",
+    price: "$75,000",
+    folder: "assets/images/cars/MERCEDES GLE 350D",
     images: [
-      "IMG_3518.JPG",
-      "IMG_3516.JPG",
-      "IMG_3521.JPG",
-      "IMG_3522.JPG",
-      "IMG_3525.JPG",
-      "IMG_3526.JPG",
-      "IMG_3527.JPG",
-      "IMG_3530.JPG",
-      "IMG_3534.JPG",
-      "IMG_3542.JPG"
+      "IMG_4001.JPG",
+      "IMG_4002.JPG"
     ]
   },
-  {
-    id: 6,
-    name: "Lexus LX600",
-    transmission: "Automatic",
-    color: "White",
-    model: "2024",
-    folder: "assets/images/cars/LEXUS LX600",
-    images: [
-      "IMG_1978.JPG",
-      "IMG_1967.JPG",
-      "IMG_1970.JPG",
-      "IMG_1971.JPG",
-      "IMG_1976.JPG",
-      "IMG_1981.JPG"
-    ]
-  },
-  {
-    id: 7,
-    name: "Toyota Harrier",
-    transmission: "Automatic",
-    color: "Pearl White",
-    model: "2017",
-    folder: "assets/images/cars/HARRIER 2017 CANON",
-    images: [
-      "IMG_3061.JPG",
-      "IMG_3069.JPG",
-      "IMG_3060.JPG",
-      "IMG_3066.JPG",
-      "IMG_3068.JPG"
-    ]
-  },
-  {
-    id: 8,
-    name: "Toyota Harrier",
-    transmission: "Automatic",
-    color: "White",
-    model: "2015",
-    folder: "assets/images/cars/Harrier Canon",
-    images: [
-      "IMG_1135.JPG",
-      "IMG_1097.JPG",
-      "IMG_1099.JPG",
-      "IMG_1101.JPG",
-      "IMG_1106.JPG",
-      "IMG_1113.JPG",
-      "IMG_1122.JPG",
-      "IMG_1130.JPG",
-      "IMG_1132.JPG"
-    ]
-  },
-  {
-    id: 9,
-    name: "Toyota Vanguard",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2012",
-    folder: "assets/images/cars/VANGUARD CANON",
-    images: [
-      "IMG_3756.JPG",
-      "IMG_3755.JPG",
-      "IMG_3757.JPG",
-      "IMG_3758.JPG",
-      "IMG_3759.JPG",
-      "IMG_3761.JPG",
-      "IMG_3762.JPG",
-      "IMG_3763.JPG"
-    ]
-  },
-  {
-    id: 10,
-    name: "Toyota Landcruiser Prado",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2025",
-    folder: "assets/images/cars/PRADO 2025 CANON",
-    images: [
-      "IMG_3960.JPG",
-      "IMG_3959.JPG",
-      "IMG_3961.JPG",
-      "IMG_3962.JPG",
-      "IMG_3963.JPG",
-      "IMG_3966.JPG",
-      "IMG_3968.JPG"
-    ]
-  },
-  {
-    id: 11,
-    name: "Mazda CX-5",
-    transmission: "Automatic",
-    color: "Blue",
-    model: "2017",
-    folder: "assets/images/cars/2017 CX-5",
-    images: [
-      "IMG_1476.JPG",
-      "IMG_1496.JPG",
-      "IMG_1498.JPG",
-      "IMG_1499.JPG",
-      "IMG_1472.JPG",
-      "IMG_1474.JPG",
-      "IMG_1477.JPG",
-      "IMG_1479.JPG",
-      "IMG_1485.JPG",
-      "IMG_1490.JPG"
-    ]
-  },
-  {
-    id: 12,
-    name: "Mazda CX-5",
-    transmission: "Automatic",
-    color: "Gray",
-    model: "2016",
-    folder: "assets/images/cars/MAZDA CX-5 2016 GRAY CANON",
-    images: [
-      "IMG_3498.JPG",
-      "IMG_3499.JPG",
-      "IMG_3500.JPG",
-      "IMG_3501.JPG",
-      "IMG_3502.JPG",
-      "IMG_3512.JPG",
-      "IMG_3514.JPG"
-    ]
-  },
-  {
-    id: 13,
-    name: "Mazda CX-5",
-    transmission: "Automatic",
-    color: "Silver",
-    model: "2016",
-    folder: "assets/images/cars/CX-5 2016 CANON",
-    images: [
-      "IMG_3373.JPG",
-      "IMG_3447.JPG",
-      "IMG_3449.JPG",
-      "IMG_3374.JPG",
-      "IMG_3375.JPG",
-      "IMG_3376.JPG"
-    ]
-  },
-  {
-    id: 14,
-    name: "Toyota Alphard",
-    transmission: "Automatic",
-    color: "Silver",
-    model: "2013",
-    folder: "assets/images/cars/ALPHARD CANON",
-    images: [
-      "IMG_3699.JPG",
-      "IMG_3702.JPG",
-      "IMG_3705.JPG",
-      "IMG_3711.JPG",
-      "IMG_3700.JPG",
-      "IMG_3701.JPG"
-    ]
-  },
-  {
-    id: 15,
-    name: "Toyota Noah",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2012",
-    folder: "assets/images/cars/BLACK NOAH CANON",
-    images: [
-      "IMG_3738.JPG",
-      "IMG_3743.JPG",
-      "IMG_3749.JPG",
-      "IMG_3737.JPG",
-      "IMG_3739.JPG",
-      "IMG_3740.JPG"
-    ]
-  },
-  {
-    id: 16,
-    name: "Toyota Corolla Fielder",
-    transmission: "Manual",
-    color: "Silver",
-    model: "2017",
-    folder: "assets/images/cars/FIELDER 2017 CANON",
-    images: [
-      "IMG_3231.JPG",
-      "IMG_3235.JPG",
-      "IMG_3279.JPG",
-      "IMG_3289.JPG",
-      "IMG_3230.JPG",
-      "IMG_3232.JPG",
-      "IMG_3233.JPG"
-    ]
-  },
-  {
-    id: 17,
-    name: "Toyota Corolla Fielder",
-    transmission: "Automatic",
-    color: "Silver",
-    model: "2013",
-    folder: "assets/images/cars/FIELDER LATEST CANON",
-    images: [
-      "IMG_2844.JPG",
-      "IMG_2833.JPG",
-      "IMG_2836.JPG",
-      "IMG_2838.JPG",
-      "IMG_2839.JPG"
-    ]
-  },
-  {
-    id: 18,
-    name: "Subaru Forester",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2012",
-    folder: "assets/images/cars/FORESTER LATEST CANON",
-    images: [
-      "IMG_2822.JPG",
-      "IMG_2828.JPG",
-      "IMG_2821.JPG",
-      "IMG_2825.JPG",
-      "IMG_2826.JPG",
-      "IMG_2827.JPG"
-    ]
-  },
-  {
-    id: 19,
-    name: "Subaru Forester",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2011",
-    folder: "assets/images/cars/FORESTER NEW BLACK CANON",
-    images: [
-      "IMG_2021.JPG",
-      "IMG_2037.JPG",
-      "IMG_2022.JPG",
-      "IMG_2024.JPG",
-      "IMG_2026.JPG",
-      "IMG_2030.JPG"
-    ]
-  },
-  {
-    id: 20,
-    name: "Toyota Wish",
-    transmission: "Automatic",
-    color: "Gold",
-    model: "2013",
-    folder: "assets/images/cars/GOLDEN WISH CANON",
-    images: [
-      "IMG_1870.JPG",
-      "IMG_1883.JPG",
-      "IMG_1884.JPG",
-      "IMG_1871.JPG",
-      "IMG_1873.JPG",
-      "IMG_1875.JPG",
-      "IMG_1879.JPG"
-    ]
-  },
-  {
-    id: 21,
-    name: "Toyota Hiace",
-    transmission: "Manual",
-    color: "White",
-    model: "2012",
-    folder: "assets/images/cars/HIACE",
-    images: [
-      "IMG_1910.JPG",
-      "IMG_1908.JPG",
-      "IMG_1912.JPG",
-      "IMG_1914.JPG",
-      "IMG_1917.JPG"
-    ]
-  },
-  {
-    id: 22,
-    name: "Subaru Impreza",
-    transmission: "Automatic",
-    color: "Silver",
-    model: "2014",
-    folder: "assets/images/cars/Impreza Canon",
-    images: [
-      "IMG_1215.JPG",
-      "IMG_1205.JPG",
-      "IMG_1211.JPG",
-      "IMG_1217.JPG",
-      "IMG_1220.JPG",
-      "IMG_1228.JPG"
-    ]
-  },
-  {
-    id: 23,
-    name: "Isuzu Forward",
-    transmission: "Manual",
-    color: "White",
-    model: "1995",
-    folder: "assets/images/cars/ISUZU FORWARD",
-    images: [
-      "IMG_1705.JPG",
-      "IMG_1696.JPG",
-      "IMG_1699.JPG",
-      "IMG_1704.JPG",
-      "IMG_1709.JPG",
-      "IMG_1710.JPG",
-      "IMG_1732.JPG",
-      "IMG_1734.JPG"
-    ]
-  },
-  {
-    id: 24,
-    name: "Mercedes Benz ML350 4Matic",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2010",
-    folder: "assets/images/cars/MERCEDES 4MATIC CANON",
-    images: [
-      "IMG_3322.JPG",
-      "IMG_3323.JPG",
-      "IMG_3325.JPG",
-      "IMG_3326.JPG",
-      "IMG_3327.JPG",
-      "IMG_3330.JPG",
-      "IMG_3333.JPG",
-      "IMG_3334.JPG",
-      "IMG_3338.JPG"
-    ]
-  },
-  {
-    id: 25,
-    name: "Subaru Outback",
-    transmission: "Automatic",
-    color: "Black",
-    model: "2011",
-    folder: "assets/images/cars/Outback Canon",
-    images: [
-      "IMG_1344.JPG",
-      "IMG_1333.JPG",
-      "IMG_1335.JPG",
-      "IMG_1337.JPG",
-      "IMG_1339.JPG",
-      "IMG_1346.JPG"
-    ]
-  },
-  {
-    id: 26,
-    name: "Toyota Passo",
-    transmission: "Automatic",
-    color: "Desert Khaki",
-    model: "2011",
-    folder: "assets/images/cars/PASSO CANON",
-    images: [
-      "IMG_2801.JPG",
-      "IMG_2799.JPG",
-      "IMG_2803.JPG",
-      "IMG_2806.JPG",
-      "IMG_2808.JPG",
-      "IMG_2810.JPG",
-      "IMG_2811.JPG",
-      "IMG_2813.JPG"
-    ]
-  },
-  {
-    id: 27,
-    name: "Toyota Probox",
-    transmission: "Automatic",
-    color: "White",
-    model: "2016",
-    folder: "assets/images/cars/PROBOX 2016 CANON",
-    images: [
-      "IMG_3300.JPG",
-      "IMG_3301.JPG",
-      "IMG_3303.JPG",
-      "IMG_3304.JPG",
-      "IMG_3306.JPG",
-      "IMG_3310.JPG",
-      "IMG_3314.JPG"
-    ]
-  },
-  {
-    id: 28,
-    name: "Toyota Rumion",
-    transmission: "Automatic",
-    color: "White",
-    model: "2012",
-    folder: "assets/images/cars/WHITE RUMION CANON",
-    images: [
-      "IMG_3571.JPG",
-      "IMG_3570.JPG",
-      "IMG_3572.JPG",
-      "IMG_3574.JPG",
-      "IMG_3576.JPG",
-      "IMG_3587.JPG"
-    ]
-  }
+  // Add more cars here...
 ];
 
 /* ---------- INVENTORY: RENDER + PAGINATION ---------- */
@@ -572,16 +131,15 @@ const cars = [
 
       inventoryGrid.appendChild(card);
 
-      // style main image
-const mainImg = card.querySelector('.main-img');
-mainImg.style.width = '720px';
-mainImg.style.maxWidth = '100%';
-mainImg.style.height = '240px';        // reduce height so full car fits
-mainImg.style.objectFit = 'contain';   // show full car without cropping
-mainImg.style.borderRadius = '10px';
-mainImg.style.display = 'block';
-mainImg.style.margin = '0 auto';
-
+      // Style main image
+      const mainImg = card.querySelector('.main-img');
+      mainImg.style.width = '720px';
+      mainImg.style.maxWidth = '100%';
+      mainImg.style.height = '240px';
+      mainImg.style.objectFit = 'contain';
+      mainImg.style.borderRadius = '10px';
+      mainImg.style.display = 'block';
+      mainImg.style.margin = '0 auto';
     });
 
     if (pageInfoEl) pageInfoEl.innerText = `Page ${currentPage}`;
@@ -594,6 +152,7 @@ mainImg.style.margin = '0 auto';
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   });
+
   if (nextPageBtn) nextPageBtn.addEventListener('click', () => {
     const maxPage = Math.ceil(cars.length / carsPerPage);
     if (currentPage < maxPage) {
@@ -605,4 +164,3 @@ mainImg.style.margin = '0 auto';
 
   renderInventory();
 })();
-
